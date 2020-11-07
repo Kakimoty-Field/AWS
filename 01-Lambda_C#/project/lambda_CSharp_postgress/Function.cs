266de8ce9c1e21ac18409e68e2ff8521bffb7580
Daichi_Kakimoto <daichi_kakimoto@saitama.fujimic.com>
2020-11-07 16:27:54 +0900

    add ru

--- 01-Lambda_C#/project/lambda_CSharp_postgress/Function.cs
+++ 01-Lambda_C#/project/lambda_CSharp_postgress/Function.cs
@@ -15,7 +15,10 @@ namespace lambda_CSharp_postgress
     {
         private string logString(string msg)
         {
-            return $"{System.DateTime.Now.ToString("yyyyMMdd-hhmm-ss.ffff")} :{msg}";
+            //            var d = new System.DateTime(DateTime.Now.Ticks, DateTimeKind.Utc);
+            //            d = d.AddHours(9);
+            var d = DateTime.Now;
+            return $"{d.ToString("yyyyMMdd-HHmm-ss.ffff")} :{msg}";
         }
 
         //
@@ -27,7 +30,7 @@ namespace lambda_CSharp_postgress
             var pass = Environment.GetEnvironmentVariable("DB_PASS") as string;
             var name = Environment.GetEnvironmentVariable("DB_NAME") as string;
 
-            context.Logger.LogLine($"Host={host};Username={user};Password={pass};Database={name};");
+            context.Logger.LogLine(logString($"Host={host};Username={user};Password={pass};Database={name};"));
             return $"Host={host};Username={user};Password={pass};Database={name};";
         }
 
@@ -38,28 +41,55 @@ namespace lambda_CSharp_postgress
             return new NpgsqlConnection(connectionString);
         }
 
-        private async Task<int> dbAccessAsync(ILambdaContext context, string connection, string query)
+        private async Task<int> dbAccessNonQueryAsync(ILambdaContext context, string connection, string query)
         {
             try
             {
-                context.Logger.LogLine(logString("Start"));
+                //                context.Logger.LogLine(logString("Start"));
                 using (var con = getConnection(connection))
                 {
-                    context.Logger.LogLine(logString("connect"));
+                    //                    context.Logger.LogLine(logString("connect"));
                     await con.OpenAsync();
                     var cmd = con.CreateCommand();
                     cmd.CommandText = query;
-                    context.Logger.LogLine(logString("Scalar"));
-                    await cmd.ExecuteScalarAsync();
-                    context.Logger.LogLine(logString("Close"));
+                    //                    context.Logger.LogLine(logString("Scalar"));
+                    await cmd.ExecuteNonQueryAsync();
+                    //                    context.Logger.LogLine(logString("Close"));
                     await con.CloseAsync();
                     await con.DisposeAsync();
                 }
-                context.Logger.LogLine(logString("End"));
+                //                context.Logger.LogLine(logString("End"));
             }
             catch (Exception e)
             {
                 context.Logger.LogLine($"dbAccessAsync Error: [{e.Message }]");
+            }
+            return 1;
+        }
+
+        private async Task<int> dbAccessAsync(ILambdaContext context, string connection, string query)
+        {
+            try
+            {
+//                context.Logger.LogLine(logString("Start"));
+                using (var con = getConnection(connection))
+                {
+//                    context.Logger.LogLine(logString("connect"));
+                    await con.OpenAsync();
+                    var cmd = con.CreateCommand();
+                    cmd.CommandText = query;
+//                    context.Logger.LogLine(logString("Scalar"));
+                    await cmd.ExecuteScalarAsync();
+//                    context.Logger.LogLine(logString("Close"));
+                    await con.CloseAsync();
+                    await con.DisposeAsync();
+                }
+//                context.Logger.LogLine(logString("End"));
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine($"dbAccessAsync Error: [{e.Message }]");
+                throw e;
             }
             return 1 ;
         }
@@ -72,17 +102,30 @@ namespace lambda_CSharp_postgress
         /// <returns></returns>
         public string FunctionHandler(object input, ILambdaContext context)
         {
+            var taskList = new List<Task<int>>();
             var retMsg = $"OK []";
             var connectionString = createConnectionString(context);
             //context.Logger.LogLine($"Arg : [{input}]");
             try
             {
-                var ret = dbAccessAsync(context,
-                                     connectionString,
-                                      $"INSERT INTO test01 (name, address, updt) VALUES ('yuka', '1', '{System.DateTime.Now.ToString("yyyyMMddhhmm-ss-fffffff")}')");
+                // 一回同期処理
+                var task = dbAccessNonQueryAsync(context, connectionString, $"INSERT INTO test01 (name, address, updt) VALUES ('yuka', '1', '{System.DateTime.Now.ToString("yyyyMMddhhmm-ss-fffffff")}')");
+                task.Wait();
 
-                retMsg = $"OK insertCount=[{ret.Result}]";
-                context.Logger.LogLine(logString($"Function End [{retMsg}]"));
+                // 非同期アクセス作成
+                context.Logger.LogLine(logString("Create Thread"));
+                //Enumerable.Range(0, 100).ToList().ForEach(x =>
+                for (var i = 0; i < 1000; i++)
+                {
+                    taskList.Add(dbAccessNonQueryAsync(context, connectionString, $"INSERT INTO test01 (name, address, updt) VALUES ('yuka', '1', '{System.DateTime.Now.ToString("yyyyMMddhhmm-ss-fffffff")}')"));
+                }
+
+                // 非同期を全部待つ
+                context.Logger.LogLine(logString("WaitThread"));
+                var newTask = Task.WhenAll(taskList.ToArray());
+                newTask.Wait();
+
+                context.Logger.LogLine(logString("...Done"));
             }
             catch (Exception e)
             {
@@ -106,21 +149,22 @@ namespace lambda_CSharp_postgress
             try
             {
                 // 一回同期処理
-                var task = dbAccessAsync(context, connectionString, $"SELECT * FROM test01 order by updt asc");
+                var task = dbAccessAsync(context, connectionString, $"SELECT count(*) FROM test01");
                 task.Wait();
-
+                
                 // 非同期アクセス作成
                 context.Logger.LogLine(logString("Create Thread"));
                 //Enumerable.Range(0, 100).ToList().ForEach(x =>
-                for(var i = 0; i < 100000; i++)
+                for(var i = 0; i < 1000; i++)
                 {
-                    taskList.Add(dbAccessAsync(context, connectionString, $"SELECT * FROM test01 order by updt asc"));
+                    taskList.Add(dbAccessAsync(context, connectionString, $"SELECT count(*) FROM test01"));
                 }
-
+                
                 // 非同期を全部待つ
                 context.Logger.LogLine(logString("WaitThread"));
                 var newTask = Task.WhenAll(taskList.ToArray());
                 newTask.Wait();
+                
                 context.Logger.LogLine(logString("...Done"));
             }
             catch (Exception e)
